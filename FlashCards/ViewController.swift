57a464db28c2e8aa91e932bd49b348568859ba22
iD Student <idstudent@stratton-one-eighty-one.mit.edu>
2017-08-01 11:58:13 -0700

    Data is now able to be displayed

--- FlashCards/ViewController.swift
+++ FlashCards/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var questionTextView: UITextView!
+    @IBOutlet weak var answerPickerView: UIPickerView!
+    @IBOutlet weak var questionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCardUI()
+        
+        answerPickerView.dataSource = self
+        answerPickerView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func setupCardUI()
+    {
+        questionTextView.text = CardCollection.instance.currentCard.question
+        questionLabel.text = "Question: \(CardCollection.instance.currentIndex + 1)/\(CardCollection.instance.cards.count)"
+    }
+    // Pickerview Data Source
+    // returns the number of 'columns' to display.
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    // returns the # of rows in each component..
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return CardCollection.instance.currentCard.options.count
+    }
+    
+    // Pickerview delegates
+    // returns text of option at a given row.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CardCollection.instance.currentCard.options[row]
+    }
+    
+    
 }
 
